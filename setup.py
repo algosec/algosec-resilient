59801d1072003305e84ec847f45ba372541dd6c3
almogcohen <almog1232@gmail.com>
2018-12-13 13:22:19 -0800

    Bump version: 1.1.0 → 1.2.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ FUNCTION_COMPONENTS = {
 
 setup(
     name='algosec_resilient',
-    version='1.1.0',
+    version='1.2.0',
     license='MIT',
     author='AlgoSec',
     author_email='support@algosec.com',
